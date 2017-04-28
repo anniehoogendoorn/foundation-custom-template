9b45cf1b44fe525133a43a873475616df2dd8308
Annie Hoogendoorn <annie.hoogendoorn@sq1.com>
2017-04-28 15:51:41 -0700

    Add files

---
+++ js/plugins.js
@@ -0,0 +1,3 @@
+// @codekit-append "../bower_components/jquery/dist/jquery.js";
+// @codekit-append "../bower_components/what-input/dist/what-input.js"";
+// @codekit-append "../bower_components/foundation-sites/dist/js/foundation.js";
